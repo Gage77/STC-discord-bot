cce5425969248c19629ded241a891e07e366c252
Hunter Black <huntergblack@gmail.com>
2018-09-09 12:02:22 -0500

    framework for new guild members

--- bot.js
+++ bot.js
@@ -6,11 +6,18 @@ client.on('ready', () => {
     console.log('Torgan Simtral has arrived at the library');
 });
 
+// Messages
 client.on('message', (message) => {
     // Ping test
     if (message.content == '*ping') {
         message.channel.send(' pong');
     }
 });
+
+// New guild member listener
+client.on('guildMemberAdd', member => {
+    // Send message to a designated channel
+    
+})
 
 client.login(botconfig.token);
