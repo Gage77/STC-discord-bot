38ca20aedf748b956d257850cc06991c6b5ce3e1
Hunter Black <huntergblack@gmail.com>
2018-09-14 16:31:57 -0500

    Moved welcome channel to const variable for ease of change

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const botconfig = require('./botconfig.json');
 
 const welcomeMessage = "Welcome to the Tabletop Gaming Club Discord! Please be sure to check out the \"rules\" channel for the general rules of the server. Additionally, if you ever have any questions about the club or the server, feel free to ask any of the officers!";
+const welcomeChannel = 'welcome'
 
 client.on('ready', () => {
     console.log('Torgan Simtral has arrived at the library');
@@ -20,7 +21,7 @@ client.on('message', (message) => {
 client.on('guildMemberAdd', member => {
     // Send message to a designated channel
     member.sendMessage(welcomeMessage);
-    const wchannel = client.channels.find('name', 'welcome');
+    const wchannel = client.channels.find('name', welcomeChannel);
     wchannel.send(`Hail, ${member}!` + welcomeMessage);
 });
 
