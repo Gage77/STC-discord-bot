0dd90b99d0c255109f1cf48ca6fdc076639d194a
Hunter Black <huntergblack@gmail.com>
2018-09-14 16:31:13 -0500

    updated welcome message to send message to 'welcome' channel

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const botconfig = require('./botconfig.json');
 
-const welcomeMessage = "test";
+const welcomeMessage = "Welcome to the Tabletop Gaming Club Discord! Please be sure to check out the \"rules\" channel for the general rules of the server. Additionally, if you ever have any questions about the club or the server, feel free to ask any of the officers!";
 
 client.on('ready', () => {
     console.log('Torgan Simtral has arrived at the library');
@@ -20,8 +20,8 @@ client.on('message', (message) => {
 client.on('guildMemberAdd', member => {
     // Send message to a designated channel
     member.sendMessage(welcomeMessage);
-    const wchannel = client.channels.find('name', welcome);
-    wchannel.send(`Hail, ${member}! Please read the Discord rules pinned in this channel. Also please read the topics of each channel before posting to said channel to be sure you remain on-topic.`);
+    const wchannel = client.channels.find('name', 'welcome');
+    wchannel.send(`Hail, ${member}!` + welcomeMessage);
 });
 
 client.login(botconfig.token);
