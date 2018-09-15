e0671ae2a0a6c1d4365ff1ac7bd214a9576d0c76
Hunter Black <huntergblack@gmail.com>
2018-09-15 15:28:29 -0500

    Updated to send message to new members DM

--- bot.js
+++ bot.js
@@ -2,9 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const botconfig = require('./botconfig.json');
 
-const welcomeMessage = "Welcome to the Tabletop Gaming Club Discord! Please be sure to check out the \"rules\" channel for the general rules of the server. Additionally, if you ever have any questions about the club or the server, feel free to ask any of the officers!";
-const welcomeChannel = 'welcome'
-
 client.on('ready', () => {
     console.log('Torgan Simtral has arrived at the library');
 });
@@ -19,10 +16,17 @@ client.on('message', (message) => {
 
 // New guild member listener
 client.on('guildMemberAdd', member => {
-    // Send message to a designated channel
-    member.sendMessage(welcomeMessage);
-    const wchannel = client.channels.find('name', welcomeChannel);
-    wchannel.send(`Hail, ${member}!` + welcomeMessage);
+    // Setup consts for referencing channels in guild
+    const ruleChannel = client.channels.find('name', 'rules');
+    const ruleChannelDM = "#" + ruleChannel;
+    const generalChannel = client.channels.find('name', 'general');
+    const generalChannelDM  = "#" + generalChannel;
+    
+    // Default welcome message
+    const welcomeMessage = "Welcome to the Tabletop Gaming Club Discord! Please be sure to check out the " + ruleChannelDM + " channel for the rules of the server. Additionally, if you ever have any questions about the club or the server, feel free to ask any of the officers! Head on over to the " + generalChannelDM + " channel to introduce yourself!";
+
+    // Send message to a members DM
+    member.sendMessage(`Hail, ${member}!` + welcomeMessage);
 });
 
 client.login(botconfig.token);
